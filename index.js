0eaeff52da74c4cc84eca4ff3cf66aa3dcc5b69d
unknown <johnnoad04@gmail.com>
2023-08-03 12:02:04 +0200

    Massive Improvements!

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const app = express()
-const port = 3000;
-const hostname = '192.168.9.23'
-const cp = require("child_process");
+const fs         = require('fs');
+const express    = require('express');
+const app        = express();
+const cp         = require("child_process");
+const bodyParser = require('body-parser');
+const port       = 3000;
+let   hostname;
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+try {
+  const ipStdOut = cp.execSync('ipconfig | find "IPv4"').toString();
+  hostname = ipStdOut.slice(39, -1).trim();
+  cp.execSync(`"http://${hostname}:${port}/" | clip`, {shell: "powershell"})
+} catch (error) {
+  console.error(error);
+}
 
 app.get('/', (req, res) => {
-  cp.exec('notepad.exe "./files/text.txt"', (error) => {
-    if (error) {
-      console.error(error)
+  fs.writeFile('./files/text.txt', req.body.msg, (err) => {
+    if (err) {
+      console.error(err)
     }
-  }); 
-  res.send('Success!')
-})
+    cp.exec('notepad.exe "./files/text.txt"', (error) => {
+      if (error) {
+        console.error(error)
+      }
+    }); 
+    res.send('Success!')
+  })
 
-app.get('/watching', (req, res) => {
-  cp.exec('notepad.exe "./files/watching.txt"', (error) => {
-    if (error) {
-      console.error(error)
-    }
-  }); 
-  res.send('Success!')
-})
-
-app.get('/feel', (req, res) => {
-  cp.exec('notepad.exe "./files/feel.txt"', (error) => {
-    if (error) {
-      console.error(error)
-    }
-  }); 
-  res.send('Success!')
 })
 
 app.listen(port, hostname, () => {
