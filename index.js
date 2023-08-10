a50875bbdedd677a28cd3a99d5ce7f22fa19a626
unknown <johnnoad04@gmail.com>
2023-08-10 13:32:51 +0200

    Live 'Video' Added

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
+// Imports & Server Variables
 const fs         = require('fs');
 const express    = require('express');
 const app        = express();
 const cp         = require("child_process");
 const bodyParser = require('body-parser');
+const screenshot = require('screenshot-desktop');
 const port       = 3000;
 let   hostname;
 
+// Setup Server To Parse Json Content On Request Bodies. 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// Grabbing IPv4 Address And Setting it As the Server Hostname 
 try {
   const ipStdOut = cp.execSync('ipconfig | find "IPv4"').toString();
-  hostname = ipStdOut.slice(39, -1).trim();
-  cp.execSync(`"http://${hostname}:${port}/" | clip`, {shell: "powershell"})
+  hostname = ipStdOut.match(/(\d+\.\d+\.\d+\.\d+)/)[0];
+  // cp.execSync(`"http://${hostname}:${port}/" | clip`, {shell: "powershell"})
 } catch (error) {
   console.error(error);
+  throw error;
 }
 
-app.get('/', (req, res) => {
+// Setting View Engine to Use EJS
+app.set('view engine', 'ejs');
+
+// Home Route
+app.get('/', async (req, res) => {
+  try {
+    // Capture the screen as an image
+    const imgBuffer = await screenshot();
+
+    const imgString = imgBuffer.toString('base64');
+    
+    // Render the EJS view with the captured image data
+    res.render('index', { imgString });
+  } catch (error) {
+    console.error('Error capturing or rendering screenshot:', error);
+    res.status(500).send('Internal Server Error');
+  }
+})
+
+// Routes
+app.get('/text', (req, res) => {
   fs.writeFile('./files/text.txt', req.body.msg, (err) => {
     if (err) {
       console.error(err)
@@ -31,6 +56,19 @@ app.get('/', (req, res) => {
   })
 
 })
+
+app.get('/updateScreenshot', async (req, res) => {
+  try {
+    // Capture a new screen screenshot
+    const newImageBuffer = await screenshot();
+    
+    // Send the updated image buffer to the client
+    res.json({ imageBuffer: newImageBuffer.toString('base64') });
+  } catch (error) {
+    console.error('Error capturing or sending updated screenshot:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
